e9bcb839c0e842d2e861244d05918416c7133366
zhaojialiang <zhaojialiang@conew.com>
2016-10-18 11:03:06 +0800

    1.use loop to instead of layout children

--- TestMemory/customadapterview/src/main/java/com/google/customadapterview/AbsAdapterView.java
+++ TestMemory/customadapterview/src/main/java/com/google/customadapterview/AbsAdapterView.java
@@ -76,46 +76,19 @@ public class AbsAdapterView extends AdapterView {
             return;
         }
 
-////        mInLayout = true;
-//        final int adapterCount = mAdapter.getCount();
-//        if (adapterCount == 0) {
-//            removeAllViewsInLayout();
-////            removeAndAddToCache(0);
-//        } else {
-////            removeAndAddToCache(0);
-//            layoutChildren(0, adapterCount);
-//        }
-        View cache0 = getChildAt(0);
-        View newUnderChild0 = mAdapter.getView(0, cache0, this);
-        if (cache0 == null) {
-            addViewInLayout(newUnderChild0, 0, newUnderChild0.getLayoutParams());
+        int delta = 0;
+        for (int i = 0; i < 3 ; i++) {
+            View cache = getChildAt(i);
+            View newUnderChild = mAdapter.getView(i, cache, this);
+            if (cache == null) {
+                addViewInLayout(newUnderChild, i, newUnderChild.getLayoutParams());
+            }
+            int childWidthSpec = getChildMeasureSpec(widthMeasureSpec, getPaddingTop() + getPaddingBottom(), newUnderChild.getLayoutParams().width);
+            int childHeightSpec = getChildMeasureSpec(heightMeasureSpec, getPaddingLeft() + getPaddingRight(), newUnderChild.getLayoutParams().height);
+            newUnderChild.measure(childWidthSpec, childHeightSpec);
+            newUnderChild.layout(0,delta,newUnderChild.getMeasuredWidth(), delta + newUnderChild.getMeasuredHeight());
+            delta += newUnderChild.getMeasuredHeight() + 5;
         }
-        int childWidthSpec = getChildMeasureSpec(widthMeasureSpec, getPaddingTop() + getPaddingBottom(), newUnderChild0.getLayoutParams().width);
-        int childHeightSpec = getChildMeasureSpec(heightMeasureSpec, getPaddingLeft() + getPaddingRight(), newUnderChild0.getLayoutParams().height);
-        newUnderChild0.measure(childWidthSpec, childHeightSpec);
-        newUnderChild0.layout(0,0,newUnderChild0.getMeasuredWidth(),newUnderChild0.getMeasuredHeight());
-
-        View cache1 = getChildAt(1);
-        View newUnderChild1 = mAdapter.getView(1, cache1, this);
-        if (cache0 == null) {
-            addViewInLayout(newUnderChild1, 1, newUnderChild1.getLayoutParams());
-        }
-        int childWidthSpec1 = getChildMeasureSpec(widthMeasureSpec, getPaddingTop() + getPaddingBottom(), newUnderChild1.getLayoutParams().width);
-        int childHeightSpec1 = getChildMeasureSpec(heightMeasureSpec, getPaddingLeft() + getPaddingRight(), newUnderChild1.getLayoutParams().height);
-        newUnderChild1.measure(childWidthSpec1, childHeightSpec1);
-        newUnderChild1.layout(0,newUnderChild0.getMeasuredHeight() + 5, newUnderChild1.getMeasuredWidth(),
-                newUnderChild0.getMeasuredHeight() + 5 + newUnderChild1.getMeasuredHeight());
-
-        View cache2 = getChildAt(2);
-        View newUnderChild2 = mAdapter.getView(2, cache2, this);
-        if (cache0 == null) {
-            addViewInLayout(newUnderChild2, 2, newUnderChild2.getLayoutParams());
-        }
-        int childWidthSpec2 = getChildMeasureSpec(widthMeasureSpec, getPaddingTop() + getPaddingBottom(), newUnderChild2.getLayoutParams().width);
-        int childHeightSpec2 = getChildMeasureSpec(heightMeasureSpec, getPaddingLeft() + getPaddingRight(), newUnderChild2.getLayoutParams().height);
-        newUnderChild2.measure(childWidthSpec2, childHeightSpec2);
-        newUnderChild2.layout(0,newUnderChild0.getMeasuredHeight() + 5 + newUnderChild1.getMeasuredHeight() + 5,
-                newUnderChild2.getMeasuredWidth(), newUnderChild0.getMeasuredHeight() + 5 + newUnderChild2.getMeasuredHeight() + 5 + newUnderChild2.getMeasuredHeight());
     }
 
     @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
