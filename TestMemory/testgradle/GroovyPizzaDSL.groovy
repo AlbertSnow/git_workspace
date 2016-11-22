15fc755e8f46572d5141afb086efc855a7b3af82
zhaojialiang <zhaojialiang@conew.com>
2016-11-22 19:42:23 +0800

    1.add groovy MOP,List,Map example

--- TestMemory/testgradle/GroovyPizzaDSL.groovy
+++ TestMemory/testgradle/GroovyPizzaDSL.groovy
@@ -1,20 +1,20 @@
-    def large = 'large'
-    def thin = 'thin'
-    def visa = 'Visa'
-    def Olives = 'Olives'
-    def Onions = 'Onions'
-    def Bell_Pepper = 'Bell Pepper'
-    orderInfo = [:]
+def large = 'large'
+def thin = 'thin'
+def visa = 'Visa'
+def Olives = 'Olives'
+def Onions = 'Onions'
+def Bell_Pepper = 'Bell Pepper'
+orderInfo = [:]
 
-    def methodMissing(String name, args) {
-        orderInfo[name] = args
-    }
+def methodMissing(String name, args) {
+    orderInfo[name] = args
+}
 
-    def acceptOrder(closure) {
-        closure.delegate = this
-        closure()
-        println "Validation and processing performed here for order received:"
-        orderInfo.each { key, value ->
-            println "${key} -> ${value.join(', ')}"
-        }
+def acceptOrder(closure) {
+    closure.delegate = this
+    closure()
+    println "Validation and processing performed here for order received:"
+    orderInfo.each { key, value ->
+        println "${key} -> ${value.join(', ')}"
     }
+}
